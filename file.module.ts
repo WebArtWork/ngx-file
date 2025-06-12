4f8f7a829f4d18904d1179a58f676d528427e3f1
Nika <danzi6386@gmail.com>
2025-06-12 18:39:06 +0300

    bring back standaloneless code

--- file.module.ts
+++ file.module.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ImageCropperModule } from 'ngx-image-cropper';
-import { ButtonComponent } from '../button/button.component';
-import { FileCropperComponent } from './file-cropper/file-cropper.component';
 import { FileComponent } from './file.component';
-
+import { CommonModule } from '@angular/common';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { FileCropperComponent } from './file-cropper/file-cropper.component';
+import { ButtonModule } from '../button/button.module';
 
 @NgModule({
 	declarations: [FileCropperComponent, FileComponent],
 	exports: [FileComponent],
-	imports: [ImageCropperModule, CommonModule, ButtonComponent]
+	imports: [ImageCropperModule, CommonModule, ButtonModule]
 })
 export class FileModule {}
